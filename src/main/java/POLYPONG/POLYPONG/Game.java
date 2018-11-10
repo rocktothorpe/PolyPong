7ce9c87439a165a68f24d408eaca8ae99d8b38ca
nathanmacfarlane <nathanmmacfarlane@gmail.com>
2018-11-10 13:51:36 -0800

    Refactor for code complexity

---
+++ src/main/java/POLYPONG/POLYPONG/Game.java
@@ -0,0 +1,73 @@
+package POLYPONG.POLYPONG;
+
+import javafx.animation.Timeline;
+import javafx.geometry.Bounds;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+public class Game {
+	
+	public final String GAMETITLE = "PolyPong";
+	public final double GAMEWIDTH = 900.0;
+	public final double GAMEHEIGHT = 500.0;
+	public final double PADDLEHEIGHT = 100.0;
+	public final double BALLRADIUS = 10.0;
+	public final double BALLSPEED = -1.05;
+	public final Color BACKGROUNDCOLOR = Color.rgb(29, 32, 44);
+	public final double STATUSBARHEIGHT = 40.0;
+	public final String P1DOWN = "S";
+	public final String P1UP = "W";
+	public final String P2DOWN = "DOWN";
+	public final String P2UP = "UP";
+	public final String PAUSEBUTTON = "P";
+	
+	public Ball ball;
+	public Paddle p1Paddle;
+	public Paddle p2Paddle;
+	public StatusBar statusBar;
+	
+	public Game() {
+		this.ball = new Ball(this.BALLRADIUS, Color.WHITE);
+        ball.relocate(this.GAMEWIDTH/2, this.GAMEHEIGHT/2 - this.BALLRADIUS/2);
+        this.p1Paddle = new Paddle(5, this.GAMEHEIGHT/2 - this.PADDLEHEIGHT/2, 10, this.PADDLEHEIGHT, Color.WHITE);
+        this.p2Paddle = new Paddle(this.GAMEWIDTH - 15, this.GAMEHEIGHT/2 - this.PADDLEHEIGHT/2, 10, this.PADDLEHEIGHT, Color.WHITE);
+        this.statusBar = new StatusBar(this.GAMEWIDTH, this.STATUSBARHEIGHT);
+	}
+	
+	public void movePaddles() {
+    	if (this.p1Paddle.isLowering) {
+    		this.p1Paddle.slideDown(this.GAMEHEIGHT);
+    	} else if (this.p1Paddle.isRaising) {
+    		this.p1Paddle.slideUp(this.STATUSBARHEIGHT);
+    	}
+    	if (this.p2Paddle.isLowering) {
+    		this.p2Paddle.slideDown(this.GAMEHEIGHT);
+    	} else if (this.p2Paddle.isRaising) {
+    		this.p2Paddle.slideUp(this.STATUSBARHEIGHT);
+    	}
+    }
+	
+	public void checkCollisions(Pane canvas, Timeline timeline) {
+    	if (this.ball.hitLeftWall()) {
+        	if (this.p1Paddle.ballCollides(this.ball)) {
+        		this.ball.xVelocity *= this.BALLSPEED;
+        	} else {
+        		timeline.pause();
+        	}
+        } else if (this.ball.hitRightWall(this.GAMEWIDTH)) {
+        	if (this.p2Paddle.ballCollides(this.ball)) {
+        		this.ball.xVelocity *= this.BALLSPEED;
+        	} else {
+        		timeline.pause();
+        	}
+        }
+    	Bounds bounds = canvas.getBoundsInLocal();
+        
+        if((this.ball.getLayoutY() >= (bounds.getMaxY() - this.ball.getRadius())) || 
+                (this.ball.getLayoutY() <= (bounds.getMinY() + this.STATUSBARHEIGHT + this.ball.getRadius()))){
+        	this.ball.yVelocity = -this.ball.yVelocity;
+        }
+    }
+	
+	
+}
--- src/main/java/POLYPONG/POLYPONG/StatusBar.java
+++ src/main/java/POLYPONG/POLYPONG/StatusBar.java
@@ -1,23 +1,46 @@
 package POLYPONG.POLYPONG;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import javafx.event.EventHandler;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 
 public class StatusBar extends HBox {
 	
-	Rectangle bar;
-	
-	public StatusBar(double gameWidth, double statusBarHeight, Color color) {
+	public StatusBar(double gameWidth, double statusBarHeight) {
 		super ();
-		bar = new Rectangle(0, 0, gameWidth, statusBarHeight);
-		bar.setFill(color);
+		this.setStyle("-fx-background-color: rgb(166, 161, 159);");
+		this.setPrefWidth(gameWidth);
 		
-//		final ImageView imv = new ImageView();
-//        final Image image2 = new Image("file:pause.png");
-//        imv.setImage(image2);
+		final ImageView imv = new ImageView();
+		imv.setX(50); 
+		imv.setY(25); 
+		imv.setFitHeight(50); 
+		imv.setFitWidth(50); 
+        Image image2 = null;
+		try {
+			image2 = new Image(new FileInputStream("pause.png"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} 
+		ColorAdjust colorAdjust = new ColorAdjust();
+		colorAdjust.setBrightness(1.0);
+		imv.setEffect(colorAdjust);
+        imv.setImage(image2);
+        
+        imv.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                System.out.println("Tile pressed ");
+                event.consume();
+            }
+       });
 		
 		
-		this.getChildren().add(bar);
+		this.getChildren().addAll(imv);
 	}
 	
 }
