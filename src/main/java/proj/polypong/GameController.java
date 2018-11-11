cd592ccff2597b974248ac49eed4c1e1d83709c7
Stoyan Shukerov <stoichik@gmail.com>
2018-11-10 16:52:40 -0800

    Added menu class and a bunch of other smelly code.

--- src/main/java/proj/polypong/GameController.java
+++ src/main/java/proj/polypong/GameController.java
@@ -1,19 +1,23 @@
 package proj.polypong;
 import javafx.application.Application;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class GameController extends Application {
 	
 	public static final double GAMEWIDTH = 900.0;
 	public static final double GAMEHEIGHT = 500.0;
+	public static final Color BACKGROUNDCOLOR = Color.rgb(29, 32, 44);
 	public static final String GAMETITLE = "PolyPong";
     @Override
     public void start(Stage stage) {
     	    	
     	stage.setTitle(GAMETITLE);
     	
-    	final Game game = new Game(GAMEWIDTH, GAMEHEIGHT);
-    	game.runGame(stage);
+    	final Menu menu = new Menu(GAMEWIDTH, GAMEHEIGHT, BACKGROUNDCOLOR);
+    	
+    	menu.drawMainMenu(stage);
+    	
     }
     
     public static void main(String[] args) {
---
+++ src/main/java/proj/polypong/Menu.java
@@ -0,0 +1,47 @@
+package proj.polypong;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Menu {
+	
+	public double gamewidth;
+	public double gameheight;
+	public Color backgroundcolor;
+	
+	public Menu(double gameWidth, double gameHeight, Color backgroundColor) {
+		gamewidth = gameWidth;
+		gameheight = gameHeight;
+		backgroundcolor = backgroundColor;
+	}
+	
+	public void drawMainMenu(Stage stage) {
+		// declare main menu buttons
+		Button playGame = new Button("Play Game");
+		Button viewScoreboard = new Button("Scoreboard");
+		Button Settings  = new Button("Settings");
+		
+		// scene and pane declared and set
+		Pane canvas = new Pane();
+        Scene scene = new Scene(canvas, gamewidth, gameheight, Color.RED);
+        
+        playGame.setOnAction(new EventHandler<ActionEvent>() {
+    		public void handle(ActionEvent even ) {
+    			final Game game = new Game(gamewidth, gameheight);
+    	    	game.runGame(stage);
+    		}
+    	});
+        
+        HBox menuBox = new HBox();
+        menuBox.getChildren().addAll(playGame, viewScoreboard, Settings);
+        canvas.getChildren().addAll(menuBox);
+        stage.setScene(scene);
+        stage.show();
+	}
+}
