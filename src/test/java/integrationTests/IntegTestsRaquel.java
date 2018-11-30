ff4be941d36a27aeeb4252bdbf56f369d917cdd0
Raquel Bonilla <raquelb.2014@gmail.com>
2018-11-30 12:41:54 -0800

    integration tests

---
+++ src/test/java/integrationTests/IntegTestsRaquel.java
@@ -0,0 +1,41 @@
+/*
+ * Raquel Bonilla's 2 Integration Tests
+ * 
+ */
+package integrationTests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import proj.polypong.SettingsValues;
+
+public class IntegTestsRaquel {
+	@Test
+	public void testSetAndGetPaddleSizeSmall() {
+		
+		SettingsValues set = new SettingsValues();
+		String size = "small";
+		set.setPaddleSize(size);
+		double getsize = set.getPaddleSize();
+		
+		double answer = 50.0;
+		
+		assertEquals(answer, getsize, 0.01);
+	}
+	
+	@Test
+	public void testSetAndGetPaddleSizeMedium() {
+		
+		SettingsValues set = new SettingsValues();
+		String size = "medium";
+		set.setPaddleSize(size);
+		double getsize = set.getPaddleSize();
+		
+		double answer = 75.0;
+		
+		assertEquals(answer, getsize, 0.01);
+	}
+
+}
