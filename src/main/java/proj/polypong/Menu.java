3e01049ce4d9c2fabd8aaa92c709f8e501fe7271
Uguisu3 <john.nightingale410@gmail.com>
2018-11-30 15:30:27 -0800

    Putting in the main menu logo closes #11

--- src/main/java/proj/polypong/Menu.java
+++ src/main/java/proj/polypong/Menu.java
@@ -1,12 +1,18 @@
 package proj.polypong;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.*;
 
 public class Menu extends  Window {
 	
@@ -19,10 +25,18 @@ public class Menu extends  Window {
 
 	@Override
 	public Scene drawWindow() {
-		this.setStyle("-fx-background-color: #22262f;");
+		this.setStyle("-fx-background-color: #005f00;");
 		Button playGame = new Button("Play Game");
 		Button viewScoreboard = new Button("Scoreboard");
 		Button settings  = new Button("Settings");
+		Rectangle logo = new Rectangle(400,200);
+		try {
+			Image logoImage = new Image(new FileInputStream("images/Logo.png"));
+			logo.setFill(new ImagePattern(logoImage));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("file was not found - oh no");
+		}
 		
         Scene scene = new Scene(this, width, height, Color.BLACK);
         
@@ -43,12 +57,14 @@ public class Menu extends  Window {
     			gc.changetoScene("Scoreboard");
     		}
     	});
-        
+        VBox logoBox = new VBox();
         VBox menuBox = new VBox();
+        logoBox.setAlignment(Pos.TOP_CENTER);
+        logoBox.getChildren().addAll(logo);
         menuBox.setAlignment(Pos.CENTER);
-        menuBox.setSpacing(10.0);
+        menuBox.setSpacing(20.0);
         menuBox.getChildren().addAll(playGame, viewScoreboard, settings);
-        this.getChildren().addAll(menuBox);
+        this.getChildren().addAll(logoBox, menuBox);
         this.setAlignment(Pos.CENTER);
         return scene;
 	}
