b6e9235c35ce40ac472ec229f8c13f1f748cbabf
Alec <alecvwilliams@hotmail.com>
2018-12-03 15:51:11 -0800

    Finished Alec's integration tests

--- src/test/java/integrationTests/IntegTestsAlec.java
+++ src/test/java/integrationTests/IntegTestsAlec.java
@@ -1,53 +1,40 @@
 /*
- * Nathan Macfarlane's 2 Integration Tests
+ * Alec Williams' 2 Integration Tests
  * 
  */
 package integrationTests;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import proj.polypong.Game;
 import proj.polypong.GameController;
-import proj.polypong.NewScore;
 import proj.polypong.Settings;
 import proj.polypong.SettingsValues;
 
 public class IntegTestsAlec {
 	@Test
-	public void testMovePaddles() {
+	public void testBallSpeedMedium() {
 		
-		double paddleStartLoc, paddleEndLoc;
 		GameController gc = new GameController();
 		SettingsValues set = new SettingsValues();
 
-		Settings s = new Settings(gc, set);
 		String ballSpeed = "medium";
 		set.setBallSpeed(ballSpeed);
 		
-		//String size = "large";
-		//set.setPaddleSize(size);
-		//gc.changetoScene("Game");
-		//Game g = new Game(gc,set);
-	
-		//NewScore s= new NewScore(gc);
-		//s.drawWindow();
-		/**
-		//set paddle starting location
-		paddleStartLoc = g.p1Paddle.getY();
-		g.p1Paddle.isLowering = true;
-		g.movePaddles();
-		g.p1Paddle.isLowering = false;
-		paddleEndLoc = g.p1Paddle.getY();
+		assertEquals(set.getBallSpeed(),-1.2,0.01);		
+	}
 
-		**/
-		assertTrue(s.windowTitle.equals(gc.GAMETITLE));
+	@Test
+	public void testBallSpeedFast() {
 		
+		GameController gc = new GameController();
+		SettingsValues set = new SettingsValues();
+
+		String ballSpeed = "fast";
+		set.setBallSpeed(ballSpeed);
+		
+		assertEquals(set.getBallSpeed(),-1.25,0.01);		
 	}
 
 }
