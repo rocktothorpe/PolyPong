06d6800f1618609d614279d918cf97c8dfbf3081
Stoyan Shukerov <stoichik@gmail.com>
2018-12-03 22:50:02 -0800

    Adding a new logo. Fixing up styling on main menu

--- src/main/java/proj/polypong/Menu.java
+++ src/main/java/proj/polypong/Menu.java
@@ -1,26 +1,18 @@
 package proj.polypong;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.ImagePattern;
-import javafx.scene.shape.*;
-import java.util.logging.*;
 
 public class Menu extends  Window {
 	
-	private GameController gc;
-	private static final Logger LOGGER =  
-            Logger.getLogger(Logger.GLOBAL_LOGGER_NAME); 
+	private GameController gc; 
 	public Menu(GameController gc) {
 		super();
 		this.gc = gc;
@@ -42,15 +34,8 @@ public class Menu extends  Window {
 		Button tutorial = new Button("");
 		tutorial.getStyleClass().remove("button");
 		tutorial.getStyleClass().addAll("mainmenu-button", "tutorial-button");
-		Rectangle logo = new Rectangle(400,200);
-		try {
-			Image logoImage = new Image(new FileInputStream("images/Logo2.png"));
-			logo.setFill(new ImagePattern(logoImage));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			LOGGER.log(Level.WARNING,"file was not found - oh no");
-		}
-		
+		Label logo = new Label("");
+		logo.getStyleClass().add("logo");
         Scene scene = new Scene(this, width, height, Color.BLACK);
         
         playGame.setOnAction(new EventHandler<ActionEvent>() {
@@ -83,7 +68,7 @@ public class Menu extends  Window {
         logoBox.getChildren().addAll(logo);
         menuBox.setAlignment(Pos.CENTER);
         menuBox.setSpacing(10.0);
-        menuBox.getChildren().addAll(playGame,tutorial, viewScoreboard, settings);
+        menuBox.getChildren().addAll(playGame, viewScoreboard, settings, tutorial);
         this.getChildren().addAll(logoBox, menuBox);
         this.setAlignment(Pos.CENTER);
         return scene;
