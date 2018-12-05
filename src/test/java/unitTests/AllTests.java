9e39a2bd983ee1f0288eec5bc62d4c2f25956eb6
zpdeng <allendeng4@gmail.com>
2018-12-04 16:24:15 -0800

    added Allen's test to AllTest file

--- src/test/java/unitTests/AllTests.java
+++ src/test/java/unitTests/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ TestsAlec.class, TestsNathan.class, TestsRaquel.class, TestsStoyan.class, TestsJohn.class })
+@SuiteClasses({ TestsAlec.class, TestsNathan.class, TestsRaquel.class, TestsStoyan.class, TestsJohn.class, TestsAllen.class })
 public class AllTests {
 
 }
